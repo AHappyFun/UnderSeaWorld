d356397b607ab01cec3d6b59fc8e715e47523a94
Jeff <zl1571098645@163.com>
2020-06-21 12:59:17 +0800

    【修复ChangePrefab工具Unity再启动不见了】

--- Assets/Editor/ChangePrefab.cs
+++ Assets/Editor/ChangePrefab.cs
@@ -7,23 +7,27 @@ using System.Collections.Generic;
 /// 注：通过选中匹配搜索被替换目标
 /// （被选中物体的所有物体则替换）
 /// </summary>
-public class Change_Prefab : EditorWindow 
+public class ChangePrefab : EditorWindow
 {
- 
+
     [MenuItem("Tools/Change Prefab")]
     public static void Open()
     {
-        EditorWindow.GetWindow(typeof(Change_Prefab));
+        //GetWindow(typeof(Change_Prefab));
+        GetWindow<ChangePrefab>("ChangePrefab工具");
     }
- 
+
     public GameObject newPrefab;
-    static GameObject tonewPrefab;
- 
+    GameObject tonewPrefab;
+    bool isChange = false;
+
     void OnGUI()
     {
- 
-        newPrefab = (GameObject)EditorGUILayout.ObjectField(newPrefab, typeof(GameObject),true, GUILayout.MinWidth(100f));
-        tonewPrefab = newPrefab;
+
+        newPrefab = EditorGUILayout.ObjectField(newPrefab, typeof(Object), true, GUILayout.MinWidth(100f)) as GameObject;
+        if (newPrefab != null)
+            tonewPrefab = newPrefab;
+
         if (isChange)
         {
             GUILayout.Button("正在替换...");
@@ -34,29 +38,29 @@ public class Change_Prefab : EditorWindow
                 Change();
         }
     }
- 
-    static bool isChange = false;
- 
-    public static void Change()
+
+
+
+    public void Change()
     {
         if (tonewPrefab == null)
             return;
- 
+
         isChange = true;
         List<GameObject> destroy = new List<GameObject>();
         Object[] labels = Selection.gameObjects;
         foreach (Object item in labels)
         {
             GameObject tempGO = (GameObject)item; // (GameObject)item;
-            //只要搜到的物体包含新Prefab的名字，就会被替换
-                GameObject newGO = PrefabUtility.InstantiatePrefab(tonewPrefab) as GameObject;
-                newGO.transform.SetParent(tempGO.transform.parent);
-                newGO.name = newGO.name;
-                newGO.transform.localPosition = tempGO.transform.localPosition;
-                newGO.transform.localRotation = tempGO.transform.localRotation;
-                newGO.transform.localScale = tempGO.transform.localScale;
- 
-                destroy.Add(tempGO);
+                                                  //只要搜到的物体包含新Prefab的名字，就会被替换
+            GameObject newGO = PrefabUtility.InstantiatePrefab(tonewPrefab) as GameObject;
+            newGO.transform.SetParent(tempGO.transform.parent);
+            newGO.name = newGO.name;
+            newGO.transform.localPosition = tempGO.transform.localPosition;
+            newGO.transform.localRotation = tempGO.transform.localRotation;
+            newGO.transform.localScale = tempGO.transform.localScale;
+
+            destroy.Add(tempGO);
         }
         foreach (GameObject item in destroy)
         {
